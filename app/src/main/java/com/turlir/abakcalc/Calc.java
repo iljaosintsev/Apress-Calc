f3eb0ee709155b85f7662d0d22a5dbd18a865c07
turlir <iljaosincev@gmail.com>
2017-03-29 20:49:23 +0500

    add calculator logic and test

---
+++ app/src/main/java/com/turlir/abakcalc/Calc.java
@@ -0,0 +1,242 @@
+package com.turlir.abakcalc;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+/**
+ * Калькулятор по принципу обратной польской нотации
+ * не выводит непосредственно строку в нотации, вычисляет "на ходу"
+ */
+class Calc {
+
+    private final Pattern mPattern;
+
+    Calc() {
+        mPattern = Pattern.compile("[0-9]+"); // only digit
+    }
+
+    /**
+     * Получить ответ по строке в инфиксной форме
+     * @param input операторы и операнды
+     * @return ответ
+     * @exception RuntimeException в случае ошибки
+     */
+    Double calc(String input) throws RuntimeException {
+        Stack<Operator> operators = new Stack<>();
+        Stack<Double> numbers = new Stack<>();
+
+        String[] tokens = input.split(" ");
+        boolean isLastOperator = true; // flag for detect unar minus
+
+        for (String token : tokens) {
+            boolean isNumber = mPattern.matcher(token).find();
+            if (isNumber) { // операнд
+                numbers.push(Double.parseDouble(token));
+                isLastOperator = false;
+
+            } else if (token.equals("(")) { // открыв. скобка
+                operators.push(Operator.CS);
+                isLastOperator = true;
+
+            } else if(token.equals(")")) { // (2 + 3) расчет скобки
+                Operator tmp = null;
+                while (!operators.isEmpty()) {
+                    tmp = operators.pop();
+                    if (tmp != Operator.CS) {
+                        operateFirst(tmp, numbers);
+
+                    } else {
+                        break;
+                    }
+                }
+                if (tmp == null || tmp != Operator.CS) { // last
+                    throw new RuntimeException("Неправильно расставлены скобки");
+                }
+                isLastOperator = false;
+
+            } else { // оператор
+                Operator current = Operator.find(token);
+                if (current != null) {
+                    Operator prev;
+                    for (int i = operators.size() - 1; i >= 0 ; i--) { // идем с конца стека
+                        prev = operators.elementAt(i); // смотрим элемент
+                        if (prev == Operator.CS) { // 4 * ( 1 + 2 )
+                            break;
+                        }
+
+                        // для отличия минуса от унарного
+                        boolean p = isLastOperator && (current == Operator.REMOVE);
+                        // если приоритет prev в стеке выше приоритета current текущего
+                        // и текущий оператор минус или предыдущий токен - операнд
+                        if (!p &&  (prev.priority() > current.priority())) {
+                            operators.pop(); // компенсируем elementAt
+                            operate(numbers, prev);
+                        } else if (prev.priority() == current.priority()) {
+                            if (prev.associate()) {
+                                operators.pop(); // аналогично
+                                operate(numbers, prev);
+                            }
+                        }
+                    }
+
+                    if (current == Operator.REMOVE && isLastOperator) { // унарный минус
+                        operators.push(Operator.MULTIPLY);
+                        numbers.push(-1.0);
+                    } else { // обычный оператор
+                        operators.push(current);
+                        isLastOperator = true;
+                    }
+                }
+            }
+        }
+
+        if (operators.contains(Operator.CS)) {
+            throw new RuntimeException("Неправильно расставлены скобки");
+        }
+
+        while (!operators.isEmpty()) { // 2 + 3
+           Operator current = operators.pop();
+           operateFirst(current, numbers);
+        }
+
+        return numbers.pop();
+    }
+
+    private void operateFirst(Operator op, Stack<Double> numbers) {
+        Double result;
+        Double first = numbers.pop();
+        Double last = numbers.pop();
+        result = op.apply(last, first); // считаем приоритетную операцию
+        numbers.push(result);
+    }
+
+    private void operate(Stack<Double> numbers, Operator tmp) {
+        Double result;
+        Double first = numbers.pop();
+        Double last = numbers.pop();
+        result = tmp.apply(last, first); // считаем приоритетную операцию
+        numbers.push(result);
+    }
+
+
+    private enum Operator {
+
+        /*EX("^") {
+            @Override
+            public int priority() {
+                return 4;
+            }
+        },*/
+
+        MULTIPLY("*") {
+            @Override
+            public int priority() {
+                return 3;
+            }
+
+            @Override
+            public Double apply(Double left, Double right) {
+                return left * right;
+            }
+        },
+
+        DIVIDE("/") {
+            @Override
+            public int priority() {
+                return 3;
+            }
+
+            @Override
+            public Double apply(Double left, Double right) {
+                if (right == 0) {
+                    throw new ArithmeticException("Деление на ноль");
+                }
+                return left / right;
+            }
+        },
+
+        ADD("+") {
+            @Override
+            public int priority() {
+                return 2;
+            }
+
+            @Override
+            public Double apply(Double left, Double right) {
+                return left + right;
+            }
+        },
+
+        REMOVE("-") {
+            @Override
+            public int priority() {
+                return 2;
+            }
+
+            @Override
+            public Double apply(Double left, Double right) {
+                return left - right;
+            }
+
+            @Override
+            public boolean associate() {
+                return true;
+            }
+        },
+
+        OS("(") {
+            @Override
+            public int priority() {
+                return 1;
+            }
+
+            @Override
+            public Double apply(Double left, Double right) {
+                return null;
+            }
+        },
+
+        CS(")") {
+            @Override
+            public int priority() {
+                return 1;
+            }
+
+            @Override
+            public Double apply(Double left, Double right) {
+                return null;
+            }
+        };
+
+        private final String mOperator;
+
+        private static final Map<String, Operator> map;
+        static {
+            map = new HashMap<>();
+            for (Operator sign : Operator.values()) {
+                map.put(sign.mOperator, sign);
+            }
+        }
+
+        public static Operator find(String sign) {
+            return map.get(sign);
+        }
+
+
+        Operator(String op) {
+            mOperator = op;
+        }
+
+        public abstract int priority();
+
+        public abstract Double apply(Double left, Double right);
+
+        public boolean associate() {
+            return false;
+        }
+
+    }
+
+}
---
+++ app/src/test/java/com/turlir/abakcalc/CalcTest.java
@@ -0,0 +1,139 @@
+package com.turlir.abakcalc;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CalcTest {
+
+    private Calc c;
+
+    @Before
+    public void setUp() {
+        c = new Calc();
+    }
+
+    @Test
+    public void simpleTest() {
+        double calc = c.calc("2 + 3");
+        assertEquals(5, calc, 0.1);
+    }
+
+    @Test
+    public void simpleBracketTest() {
+        double calc = c.calc("( 2 + 3 )");
+        assertEquals(5, calc, 0.1);
+    }
+
+    @Test
+    public void sideEffectAfterBracketTest() {
+        double calc = c.calc("( 2 + 3 ) - 3");
+        assertEquals(2, calc, 0.1);
+
+        calc = c.calc("3 * ( 2 + 3 )");
+        assertEquals(15, calc, 0.1);
+    }
+
+    @Test
+    public void simpleQueueTest() {
+        double calc = c.calc("3 + 4 * 2");
+        assertEquals(11, calc, 0.1);
+
+        calc = c.calc("2 + 3 * 4");
+        assertEquals(14, calc, 0.1);
+    }
+
+    @Test
+    public void associativeQueueTest() {
+        double calc;
+
+        calc = c.calc("1 + 4 * 2 + 3");
+        assertEquals(12, calc, 0.1);
+
+        calc = c.calc("1 * 4 + 2 + 3");
+        assertEquals(9, calc, 0.1);
+
+        calc = c.calc("5 - 1 * 2 + 3");
+        assertEquals(6, calc, 0.1);
+
+        calc = c.calc("1 + 4 - 2 + 3");
+        assertEquals(6, calc, 0.1);
+
+        calc = c.calc("5 - 3 - 1 - 2");
+        assertEquals(-1, calc, 0.1);
+    }
+
+    @Test
+    public void queueAndBracketTest() {
+        double calc = c.calc("( 1 + 2 ) * 4 + 3");
+        assertEquals("first var", 15, calc, 0.1);
+
+        calc = c.calc("4 * ( 1 + 2 ) + 3");
+        assertEquals("second var", 15, calc, 0.1);
+
+        calc = c.calc("3 + ( 1 + 2 ) * 4");
+        assertEquals("three var", 15, calc, 0.1);
+    }
+
+    @Test
+    public void associativeQueueBracketTest() {
+        double calc = c.calc("( 1 + 4 ) - 2 + 3");
+        assertEquals(6, calc, 0.1);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void bracketFailTest() {
+        c.calc("( 1 + 2 ");
+        // assertEquals(3, calc, 0.1);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void divisionByZeroTest() {
+        c.calc("3 / ( 4 - 4 )");
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void twoDivisionByZeroTest() {
+        c.calc("( 5 - 4 ) / 0");
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void threeDivisionByZeroTest() {
+        c.calc("( 5 - 4 ) / ( 3 - 3 )");
+    }
+
+    @Test
+    public void unarMinusTest() {
+        Double actual;
+
+        actual = c.calc("5 * - 3");
+        assertEquals(-15.0, actual, 0.1);
+
+        actual = c.calc("- 3 * 5");
+        assertEquals(-15.0, actual, 0.1);
+
+        actual = c.calc("- 3 * ( 5 + 5 )");
+        assertEquals(-30.0, actual, 0.1);
+
+        actual = c.calc("( 5 + 5 ) * - 3");
+        assertEquals(-30.0, actual, 0.1);
+    }
+
+    @Test
+    public void hardTest() {
+        Double actual;
+
+        actual = c.calc("1 + 2 * 5 - 5 / 22");
+        assertEquals(10.77, actual, 0.01);
+
+        actual = c.calc("1 + 2 * 5 - 5 / 22 + ( 45 + 34 ) * 3");
+        assertEquals(247.77, actual, 0.01);
+
+        actual = c.calc("1 + 2 * 5 - 5 / 22 + ( 45 + 34 ) * - 3");
+        assertEquals(-226.23, actual, 0.01);
+    }
+
+
+}
