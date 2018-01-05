28cb34e319ac45a97790902fca0d537d8b07b28e
iljaosintsev <iljaosincev@gmail.com>
2018-01-06 02:50:53 +0500

    Range

--- app/src/main/java/com/turlir/Demo.java
+++ app/src/main/java/com/turlir/Demo.java
@@ -7,44 +7,82 @@ import java.util.List;
 
 class Demo implements IDemo {
 
+    private static class Range {
+        int index;
+        private int offset;
+
+        Range(int low, int up) {
+            this.index = low;
+            this.offset = up;
+        }
+
+        void captureNext() {
+            offset++;
+        }
+
+        boolean exact(int l) {
+            return index + offset < l;
+        }
+
+        boolean exactWithoutOffset(int l) {
+            return index < l;
+        }
+
+        int sum() {
+            return index + offset;
+        }
+
+        Range cycle() {
+            return new Range(index += offset, 0);
+        }
+
+        String directSubstring(String s) {
+            return s.substring(index, index + offset);
+        }
+
+        String nextSubstring(String s) {
+            String substring = s.substring(index + offset, index + offset + 1);
+            index++;
+            return substring;
+        }
+
+    }
+
     @Override
     public List<Pair<String, String>> input(final String s) {
         final String str = s.trim();
         List<Pair<String, String>> arr = new ArrayList<>();
         int length = str.length();
-        int index = 0;
-        int offset = 0;
+        Range r = new Range(0, 0);
+
         do {
-            char c = str.charAt(index);
+            char c = str.charAt(r.index);
             boolean f = digit(c);
             boolean l; // false
             do {
-                offset++;
-                if (index + offset < length) {
-                    c = str.charAt(index + offset);
+                r.captureNext();
+                if (r.exact(length)) {
+                    c = str.charAt(r.sum());
                     l = digit(c);
                 } else {
                     break;
                 }
             } while (f == l);
 
-            String subs = str.substring(index, index + offset);
+            String subs = r.directSubstring(str);
             if (f) {
                 arr.add(new Pair<>(TYPE_AND, subs));
-                if (index + offset < length &&
-                    operator(str.charAt(index + offset))) {
-                    String o = str.substring(index + offset, index + offset + 1);
+                if (r.exact(length) && operator(str.charAt(r.sum()))) {
+                    String o = r.nextSubstring(str);
                     arr.add(new Pair<>(TYPE_OR, o)); // односложный оператор, быстрый путь
-                    index++;
                 }
 
             } else {
                 arr.add(new Pair<>(TYPE_OR, subs)); // односложный оператор, медленный путь
             }
 
-            index += offset;
-            offset = 0;
-        } while (index < length);
+            r = r.cycle();
+        } while (r.exactWithoutOffset(length));
 
         return arr;
     }
