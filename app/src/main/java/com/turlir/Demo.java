9b37cbcb782ffe091b93e0c8083ea300d5131294
iljaosintsev <iljaosincev@gmail.com>
2018-01-06 02:26:38 +0500

    clear comments

--- app/src/main/java/com/turlir/Demo.java
+++ app/src/main/java/com/turlir/Demo.java
@@ -47,27 +47,6 @@ class Demo implements IDemo {
         } while (index < length);
 
         return arr;
-
-        /*
-        for (int index = 0; index < length; index++) {
-            char c = str.charAt(index);
-            boolean isNumber = Character.isDigit(c);
-            if (!isNumber) {
-                if (c == '-') {
-                    boolean u = unary(str, length, index);
-                    if (u) {
-                        // унарный минус
-                    } else {
-                        // обычный минус
-                    }
-                } else {
-                    // оператор
-                }
-            } else {
-                // операнд
-            }
-        }
-        */
     }
 
     private boolean operator(char c) {
@@ -76,18 +55,6 @@ class Demo implements IDemo {
 
     private static boolean digit(char c) {
         return Character.isDigit(c) || c == '.' /*|| c == ','*/;
-    }
-
-    private static boolean unary(String str, int l, int i) {
-        //final boolean unary;
-        boolean last = i == l - 1;
-        if (!last) {
-            boolean first = i == 0; // в начале выражения
-            boolean afterSymbol = !Character.isDigit(str.charAt(i - 1)); // после оператора или скобок
-            return first || afterSymbol; // унарный минус
-        } else {
-           return false; // не доделанный минус
-        }
     }
 
 }
