270168d0547e57848e301790f0932e567255a2d8
iljaosintsev <iljaosincev@gmail.com>
2018-07-11 01:19:21 +0500

    notation displaying, part 1
    - interface
    - adapter, decorator

---
+++ app/src/main/java/com/turlir/abakcalc/NotationRecyclerAdapter.java
@@ -0,0 +1,55 @@
+package com.turlir.abakcalc;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotationRecyclerAdapter extends RecyclerView.Adapter<NotationRecyclerAdapter.Holder> {
+
+    private final List<Object> mData;
+    private final LayoutInflater mInflater;
+
+    NotationRecyclerAdapter(Context cnt) {
+        mData = new ArrayList<>();
+        mInflater = LayoutInflater.from(cnt);
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = mInflater.inflate(R.layout.notation_item, parent, false);
+        return new Holder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        holder.bind(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    <T> void setItems(List<T> items) {
+        mData.clear();
+        mData.addAll(items);
+    }
+
+    static class Holder extends RecyclerView.ViewHolder {
+
+        Holder(View itemView) {
+            super(itemView);
+        }
+
+        void bind(Object visual) {
+            ((TextView) itemView).setText(visual.toString());
+        }
+    }
+
+}
